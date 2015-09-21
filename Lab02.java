d3c3fbc64f5bcb6db31def0bdbdbe9ca647dd376
Elijah Gjerdingen <elijah.gjerdingen@gmail.com>
2015-09-20 21:38:18 -0500

    First assignments

---
+++ Lab02.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab02;
+
+/**
+ *
+ * @author Elijah
+ */
+public class Lab02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        int a, b, c;
+        a = 11;
+        b = 11;
+        c = a + b;
+        System.out.println("The value of c is" + c);
+    }
+    
+}
